bc62c31a50eb43ab492c7f3327c6396616afe1b8
Cong Dao <vdao8537@conestogac.on.ca>
2020-03-02 12:17:09 -0500

    Add controller API

--- eCommerceCore/Controllers/ProductController.cs
+++ eCommerceCore/Controllers/ProductController.cs
@@ -4,44 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Session;
 
 namespace eCommerceCore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ProductController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Product
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
-            var a = HttpContext.Session.GetInt32("UserId");
-            
-            Console.WriteLine(a + " session id ");
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Product
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Product/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ eCommerceCore/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceCore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string Pricing { get; set; }
+        public string ProductName { get; set; }
+    }
+}
