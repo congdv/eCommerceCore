62842221556a1d4f8052bbd9f694677708275e19
ashna <amehta0082@conestogac.on.ca>
2020-03-05 19:24:16 -0500

    fixed ProductController to show all products and single product details

--- eCommerceCore/Controllers/ProductController.cs
+++ eCommerceCore/Controllers/ProductController.cs
@@ -14,64 +14,51 @@ namespace eCommerceCore.Controllers
     {
         private readonly AppDbContext context;
         public ProductController(AppDbContext context) => this.context = context;
-
-        //List<Product> productList = new List<Product>();
+                              
 
 
         // GET: api/product
         [HttpGet]
         public ActionResult<List<Product>> Get()
         {
-            List<Product> products = context.Products.ToList();
-            return products;
+            try
+            {
+                List<Product> product = context.Products.ToList();
+                return product;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { success = false, message = "Invalid Request" });
+            }
         }
 
         // GET: api/Product/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetProduct")]
         public ActionResult<Product> Get(int id)
         {
-            int productId =  id;
-            id.GetType();
-            var prod = context.Products.FindAsync(from p in
-                                                        context.Products
-                                                        where p.Id == productId
-                                                       select p);
-            return (Product)prod;
+            try
+            {
+                int productId = id;
+                var product = context.Products.FirstOrDefault(prod => prod.Id == id);
+
+                if (id.GetType() == typeof(System.String))
+                {
+                    return BadRequest(new { success = false, message = "Invalid Request" });
+                }
+                else if (product == null)
+                {
+                    return Ok(new { success = false, message = "Product Not Found!" });
+                }
+                else
+                {
+                    return (Product)product;
+                }
+                
+            }
+            catch(Exception)
+            {
+                return BadRequest(new { success = false, message = "Invalid Request" });
+            }
         }
-
-        /*
-        // GET: api/Product
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Product/5
-        //[HttpGet("{id}", Name = "GetProducts")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-
-
-            // POST: api/Product
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Product/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }*/
     }
 }
