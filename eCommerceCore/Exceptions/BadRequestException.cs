58a78b698b53544aaa757c44ade61b672a2b88ab
Cong Dao <vdao8537@conestogac.on.ca>
2020-03-14 19:11:11 -0400

    Added Exception User Handle

---
+++ eCommerceCore/Exceptions/BadRequestException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceCore.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+        {
+
+        }
+
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+
+        public BadRequestException(string message, Exception inner) : base(message,  inner)
+        {
+
+        }
+    }
+}
