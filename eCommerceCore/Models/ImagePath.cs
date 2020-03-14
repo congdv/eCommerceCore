43fad4558e500f867d668c7c30e1c3788a3abd38
Cong Dao <vdao8537@conestogac.on.ca>
2020-03-14 19:11:26 -0400

    Added ImagePath Model

---
+++ eCommerceCore/Models/ImagePath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceCore.Models
+{
+    public class ImagePath
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+
+        [ForeignKey("Comment")]
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
